43e944af3b32229ed632b1fdf07b07e2dda80ec0
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-07-15 01:05:15 +0600

    feat(ChatBubble): implement message display logic with time and sender info
    Add conditional rendering based on message sender and conversation type
    Include timestamp formatting for messages
    Handle group chat and direct message scenarios

--- components/Home/ChatBubble.tsx
+++ components/Home/ChatBubble.tsx
@@ -1,4 +1,4 @@
-import { IMessage } from "@/store/chat_store";
+import { IMessage, useConversationStore } from "@/store/chat_store";
 
 type ChatBubbleProps = {
 	me: any;
@@ -6,6 +6,25 @@ type ChatBubbleProps = {
 };
 
 const ChatBubble = ({me,message}:ChatBubbleProps) => {
-	return <div>ChatBubble</div>;
+	const date = new Date(message._creationTime);
+	const hour = date.getHours().toString().padStart(2, "0");
+	const minute = date.getMinutes().toString().padStart(2, "0");
+	const time = `${hour}:${minute}`
+
+	const {selectedConversation} = useConversationStore()
+	const isMember = selectedConversation?.participants.includes(message.sender._id) || false;
+	const isGroup =selectedConversation?.isGroup;
+	const fromMe =message.sender._id === me?._id;
+	const bgClass = fromMe ? "bg-green-chat" : "bg-white dark:bg-gray-primary";
+	if(!fromMe){
+		return (
+			<></>
+		)
+	}
+	else{
+		return (
+			<></>
+		)
+	}
 };
 export default ChatBubble;
