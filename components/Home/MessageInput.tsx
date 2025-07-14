0f065b519048e145736bb7b698040bbb85251edf
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-07-15 00:34:19 +0600

    feat(message-input): add emoji picker and visibility hook
    - Add emoji-picker-react dependency to package.json
    - Implement useComponentVisible custom hook for managing component visibility
    - Integrate emoji picker in MessageInput component with dark theme
    - Update message text with selected emoji while keeping picker open

--- components/Home/MessageInput.tsx
+++ components/Home/MessageInput.tsx
@@ -6,12 +6,15 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useConversationStore } from "@/convex/store/chat_store";
 import toast from "react-hot-toast";
+import useComponentVisible from "@/hooks/useComponentVisible";
+import EmojiPicker, {Theme} from "emoji-picker-react"
 
 const MessageInput = () => {
 	const [msgText, setMsgText] = useState("");
 	const sendTextMsg = useMutation(api.messages.sendTextMessage);
 	const me = useQuery(api.users.getMe);
 	const {selectedConversation} = useConversationStore()
+	const {ref, isComponentVisible, setIsComponentVisible} = useComponentVisible(false);
 	const handleSendTextMsg = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		try{
@@ -31,7 +34,19 @@ const MessageInput = () => {
 		<div className='bg-gray-primary p-2 flex gap-4 items-center'>
 			<div className='relative flex gap-2 ml-2'>
 				{/* EMOJI PICKER WILL GO HERE */}
-				<Laugh className='text-gray-600 dark:text-gray-400' />
+				<div ref={ref} onClick={() => setIsComponentVisible(true)}>
+					{isComponentVisible && (
+						<EmojiPicker
+						 theme={Theme.DARK}
+						 onEmojiClick={(emojiObject) => {
+							setMsgText(msgText + emojiObject.emoji);
+							//setIsComponentVisible(false);
+						 }}
+						 style={{ position: "absolute", bottom: "1.5rem", left: "1rem", zIndex: 50  }}
+						 />
+					)}
+					<Laugh className='text-gray-600 dark:text-gray-400 cursor-pointer' />
+				</div>
 				<Plus className='text-gray-600 dark:text-gray-400' />
 			</div>
 			<form onSubmit={handleSendTextMsg} className='w-full flex gap-3'>
